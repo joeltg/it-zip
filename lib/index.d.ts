41f6ef8c72d8a8efd5f5dbcef508cbdf19b00376
Joel Gustafson <joelg@mit.edu>
2021-08-26 12:42:21 -0400

    remove lib directory from git repo

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type ZIterable<E> = E extends Iterable<any>[] ? {
-    [k in keyof E]: E[k] extends Iterable<infer T> ? T : E[k];
-} : never;
-declare const zip: <E extends Iterable<any>[]>(...args: E) => Iterable<[...ZIterable<E>, number]>;
-export default zip;
